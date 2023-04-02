0b293d378ca2fc2623ca47d75964af637f21f138
Levan Parastashvili <levani.parastashvili@gmail.com>
2023-04-02 14:38:34 +0400

    First commit

---
+++ js.js
@@ -0,0 +1,128 @@
+const toggleButton = document.getElementById("arrow1");
+const toggleButtonh = document.getElementById("text1");
+const answerElement = document.getElementById("ans1");
+const toggleButton2 = document.getElementById("arrow2");
+const toggleButtonh2 = document.getElementById("text2");
+const answerElement2 = document.getElementById("ans2");
+const toggleButton3 = document.getElementById("arrow3");
+const toggleButtonh3 = document.getElementById("text3");
+const answerElement3 = document.getElementById("ans3");
+const toggleButton4 = document.getElementById("arrow4");
+const toggleButtonh4 = document.getElementById("text4");
+const answerElement4 = document.getElementById("ans4");
+const toggleButton5 = document.getElementById("arrow5");
+const toggleButtonh5 = document.getElementById("text5");
+const answerElement5 = document.getElementById("ans5");
+
+
+toggleButton.addEventListener("click", () => {
+    if (answerElement.style.display === "none") {
+        answerElement.style.display = "block";
+        toggleButton.style.transform = "rotate(180deg)";
+        toggleButtonh.style.fontWeight = "700";
+    } else {
+        answerElement.style.display = "none";
+        toggleButton.style.transform = "rotate(0deg)";
+        toggleButtonh.style.fontWeight = "";
+    }
+});
+toggleButtonh.addEventListener("click", () => {
+    if (answerElement.style.display === "none") {
+        answerElement.style.display = "block";
+        toggleButton.style.transform = "rotate(180deg)";
+        toggleButtonh.style.fontWeight = "700";
+    } else {
+        answerElement.style.display = "none";
+        toggleButton.style.transform = "rotate(0deg)";
+        toggleButtonh.style.fontWeight = "";
+    }
+});
+toggleButton2.addEventListener("click", () => {
+    if (answerElement2.style.display === "none") {
+        answerElement2.style.display = "block";
+        toggleButton2.style.transform = "rotate(180deg)";
+        toggleButtonh2.style.fontWeight = "700";
+    } else {
+        answerElement2.style.display = "none";
+        toggleButton2.style.transform = "rotate(0deg)";
+        toggleButtonh2.style.fontWeight = "";
+    }
+});
+toggleButtonh2.addEventListener("click", () => {
+    if (answerElement2.style.display === "none") {
+        answerElement2.style.display = "block";
+        toggleButton2.style.transform = "rotate(180deg)";
+        toggleButtonh2.style.fontWeight = "700";
+    } else {
+        answerElement2.style.display = "none";
+        toggleButton2.style.transform = "rotate(0deg)";
+        toggleButtonh2.style.fontWeight = "";
+    }
+});
+toggleButton3.addEventListener("click", () => {
+    if (answerElement3.style.display === "none") {
+        answerElement3.style.display = "block";
+        toggleButton3.style.transform = "rotate(180deg)";
+        toggleButtonh3.style.fontWeight = "700";
+    } else {
+        answerElement3.style.display = "none";
+        toggleButton3.style.transform = "rotate(0deg)";
+        toggleButtonh3.style.fontWeight = "";
+    }
+});
+toggleButtonh3.addEventListener("click", () => {
+    if (answerElement3.style.display === "none") {
+        answerElement3.style.display = "block";
+        toggleButton3.style.transform = "rotate(180deg)";
+        toggleButtonh3.style.fontWeight = "700";
+    } else {
+        answerElement3.style.display = "none";
+        toggleButton3.style.transform = "rotate(0deg)";
+        toggleButtonh3.style.fontWeight = "";
+    }
+});
+toggleButton4.addEventListener("click", () => {
+    if (answerElement4.style.display === "none") {
+        answerElement4.style.display = "block";
+        toggleButton4.style.transform = "rotate(180deg)";
+        toggleButtonh4.style.fontWeight = "700";
+    } else {
+        answerElement4.style.display = "none";
+        toggleButton4.style.transform = "rotate(0deg)";
+        toggleButtonh4.style.fontWeight = "";
+    }
+});
+toggleButtonh4.addEventListener("click", () => {
+    if (answerElement4.style.display === "none") {
+        answerElement4.style.display = "block";
+        toggleButton4.style.transform = "rotate(180deg)";
+        toggleButtonh4.style.fontWeight = "700";
+    } else {
+        answerElement4.style.display = "none";
+        toggleButton4.style.transform = "rotate(0deg)";
+        toggleButtonh4.style.fontWeight = "";
+    }
+});
+toggleButton5.addEventListener("click", () => {
+    if (answerElement5.style.display === "none") {
+        answerElement5.style.display = "block";
+        toggleButton5.style.transform = "rotate(180deg)";
+        toggleButtonh5.style.fontWeight = "700";
+    } else {
+        answerElement5.style.display = "none";
+        toggleButton5.style.transform = "rotate(0deg)";
+        toggleButtonh5.style.fontWeight = "";
+    }
+});
+toggleButtonh5.addEventListener("click", () => {
+    if (answerElement5.style.display === "none") {
+        answerElement5.style.display = "block";
+        toggleButton5.style.transform = "rotate(180deg)";
+        toggleButtonh5.style.fontWeight = "700";
+    } else {
+        answerElement5.style.display = "none";
+        toggleButton5.style.transform = "rotate(0deg)";
+        toggleButtonh5.style.fontWeight = "";
+    }
+});
+
